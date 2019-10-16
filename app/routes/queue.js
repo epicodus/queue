6a554a78b82adcb56086b682bcdbccf06e78a674
Mike Goren <mike@mortalwombat.net>
2019-10-16 16:44:33 -0700

    change background color of queue every 15 minutes

--- app/routes/queue.js
+++ app/routes/queue.js
@@ -2,6 +2,9 @@
 import Ember from 'ember';
 
 var QueueRoute = Ember.Route.extend({
+  init: function() {
+    $('.page-header').css('background-color', 'rgb(0, 179, 180)');
+  },
   model: function() {
     return this.store.filter('ticket', {
       orderBy: 'createdAt',
@@ -9,7 +12,17 @@ var QueueRoute = Ember.Route.extend({
     }, function(ticket) {
       return ticket.get('open');
     });
-  }
+  },
+
+  currentTimeMetronome: function() {
+    var interval = 900000;
+    Ember.run.later(this, function() {
+      var currentColor = $('.page-header').css('background-color');
+      var newColor = currentColor === 'rgb(242, 0, 143)' ? 'rgb(0, 179, 180)' : 'rgb(242, 0, 143)';
+      $('.page-header').css('background-color', newColor);
+      this.currentTimeMetronome();
+    }, interval);
+  }.on('init')
 });
 
 export default QueueRoute;
