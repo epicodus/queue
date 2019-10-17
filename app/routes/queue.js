8999df1709947f8fe82c7cb3ff15df5144fa5c86
Mike Goren <mike@mortalwombat.net>
2019-10-16 17:17:44 -0700

    change color based on time rather than from start of app

--- app/routes/queue.js
+++ app/routes/queue.js
@@ -15,11 +15,14 @@ var QueueRoute = Ember.Route.extend({
   },
 
   currentTimeMetronome: function() {
-    var interval = 900000;
+    var interval = 60000; // check once per minute
     Ember.run.later(this, function() {
       var currentColor = $('.page-header').css('background-color');
       var newColor = currentColor === 'rgb(242, 0, 143)' ? 'rgb(0, 179, 180)' : 'rgb(242, 0, 143)';
-      $('.page-header').css('background-color', newColor);
+      var minutes = moment().format('mm');
+      if (minutes === '00' || minutes === '15' || minutes === '30' || minutes === '45') {
+        $('.page-header').css('background-color', newColor);
+      }
       this.currentTimeMetronome();
     }, interval);
   }.on('init')
